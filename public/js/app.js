3551cbf84aaa3d7002fd395cf24e3d2077c5cdc5
Yulli Chong <yc@Yullis-MBP.hsd1.il.comcast.net>
2019-10-30 19:02:26 -0500

    linked form to database

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,23 @@
 const app = angular.module('shoppingCart', []);
 
 app.controller("MainController", ["$http", function($http) {
+
+  this.addItem = function () {
+    $http({
+      method: "POST",
+      url: "/items",
+      data: {
+        name: this.name,
+        description: this.description,
+        cost: this.cost,
+        image: this.image,
+        inStock: this.inStock
+      }
+    }).then(function(response) {
+
+    }, function(error) {
+      console.log(error);
+    });
+  };
   
 }]);
