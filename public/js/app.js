8800fd5f9d1dbe5d9899ce02e78e69da227ee637
Becka Catanzaro <becka@Beckas-MacBook-Pro.local>
2019-10-30 20:21:39 -0400

    added get, update, and delete item routes

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,18 @@
 const app = angular.module('shoppingCart', []);
 
 app.controller("MainController", ["$http", function($http) {
+  const controller = this;
+
+  this.getItems = function() {
+    $http({
+      method: "GET",
+      url: "/items"
+    }).then(function(response) {
+      controller.items = response.data;
+    }, function(error) {
+      console.log(error);
+    });
+  };
 
   this.addItem = function () {
     $http({
@@ -14,10 +26,39 @@ app.controller("MainController", ["$http", function($http) {
         inStock: this.inStock
       }
     }).then(function(response) {
-
+      console.log(response.data);
     }, function(error) {
       console.log(error);
     });
   };
-  
+
+  this.editItem = function (item) {
+    $http({
+      method: "PUT",
+      url: "/items/" + item._id,
+      data: {
+        name: this.updatedName,
+        description: this.updatedDescription,
+        cost: this.updatedCost,
+        image: this.updatedImage,
+        inStock: this.updatedStock
+      }
+    }).then(function(response) {
+      console.log(response.data);
+    }, function(error) {
+      console.log(error);
+    });
+  };
+
+  this.deleteItem = function (item) {
+    $http({
+      method: "DELETE",
+      url: "/items/" + item._id
+    }).then(function(response) {
+       controller.getItems();
+    }, function(error) {
+      console.log(error);
+    });
+  };
+
 }]);
